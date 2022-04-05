82b2f6b601ddea96382d76cc2be36dbc0f08126a
unknown <maxime.soufer@gmail.com>
2022-04-05 12:00:19 -0400

    get ready for deploy on vercel

--- api/index.js
+++ api/index.js
@@ -1,17 +1,17 @@
 
 //boiler plate to use express and open a port
 const express = require("express");
-const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors"); 
 // not sure if cors was actually needed in this scenario, but added it for good measure
+const app = require('express')();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 //gets the json data and stores it in a variable
-let restaurantList = require('./api/restaurants.json');
+let restaurantList = require('./restaurants.json');
 
 //sends the whole data if this url is visited
 app.get('/api',function(req,res){
